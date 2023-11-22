7205fcd47288de8f40491030d80619013ab43068
Skaimas <100655267+Skaimas@users.noreply.github.com>
2023-11-22 20:20:56 +0900

    Update Task_1.py

--- Лабораторная работа 3/Task_1.py
+++ Лабораторная работа 3/Task_1.py
@@ -1,23 +1,22 @@
 import numpy as np
 
 
-def product_list(n_list, product_n):
-    k = 0
-    for i in range(len(n_list)):
-        if n_list[i] == product_n:
+def product(list_, object_):
+    k = -1
+    for i in range(len(list_)):
+        if list_[i] == object_:
             return i
-
         k += 1
+        if k == len(list_):
+            return None
 
-        if k == len(n_list) + 1:
-            return "None"
 
+items_list = np.array(['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан'])
 
-g_list = np.array(["Сосиска в тесте", "Бутерброд", "Смаженка", "Компот", "Сладкий чай", "Булочка с маком", "Самса"])
-product_g1 = "Компот"
-product_g2 = "Хотдог"
-
-ask1 = product_list(g_list, product_g1)
-ask2 = product_list(g_list, product_g2)
-
-print(ask1, ask2, sep="\n")
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = product(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
+        
